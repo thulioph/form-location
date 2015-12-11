e29abf8459c2e1581fbb43fa0ee6824be895b352
thulioph <thulioph@gmail.com>
2015-12-11 18:35:45 -0300

    Adds new fields into form and get user info.

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -10,37 +10,40 @@
 angular.module('formLocationApp')
   .controller('MainCtrl', ['$scope', function ($scope) {
 
-    var geocoder, latlng, mapOptions, map, infowindow, marker, cep, logradouro, location_info;
+    // localizando o usuário
+    $scope.getUserLocation = function() {
+      var geocoder, latlng, mapOptions, map, infowindow, marker, location_info;
 
-    location_info = [];
+      location_info = {};
 
-    function getLocation() {
-      if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(success, error);
-      } else {
-        console.warn('Geolocation não é suportado pelo seu navegador.')
-      }
-    };
+      function getLocation() {
+        if (navigator.geolocation) {
+          navigator.geolocation.getCurrentPosition(success, error);
+        } else {
+          console.warn('Geolocation não é suportado pelo seu navegador.')
+        }
+      };
 
-    function success(position) {
-      geocoder = new google.maps.Geocoder();
+      function success(position) {
+        geocoder = new google.maps.Geocoder();
 
-      latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+        latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
 
-      mapOptions = {
-      zoom: 10,
-      center: latlng,
-      mapTypeId: 'roadmap'
-      }
+        mapOptions = {
+        zoom: 10,
+        center: latlng,
+        mapTypeId: 'roadmap'
+        }
 
-      map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
+        map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
 
-      infowindow = new google.maps.InfoWindow;
+        infowindow = new google.maps.InfoWindow;
 
-      // efetuando o geocode a partir da localização do usuário
-      geocoder.geocode({'latLng': latlng}, function(results, status) {
-        if (status == google.maps.GeocoderStatus.OK) {
-          if (results[0]) {
+        geocoder.geocode({'latLng': latlng}, function(results, status) {
+          console.warn(results);
+
+          if (status == google.maps.GeocoderStatus.OK) {
+            if (results[0]) {
               map.setZoom(11);
 
               marker = new google.maps.Marker({
@@ -48,36 +51,46 @@ angular.module('formLocationApp')
                 map: map
               });
 
-            cep = results[3].address_components[0].long_name;
-            logradouro = results[0].formatted_address;
+              location_info.full_address = results[0].formatted_address;
+              // location_info.zip = results[3].address_components[0].long_name;
 
-            infowindow.setContent(logradouro + cep);
-            infowindow.open(map, marker);
+              location_info.address = results[0].address_components[0].long_name;
+              location_info.neighborhood = results[0].address_components[1].long_name;
+              location_info.city = results[0].address_components[2].long_name;
+              location_info.state = results[0].address_components[4].long_name;
+              location_info.country = results[0].address_components[5].long_name;
 
-            // $scope.location_info.data = logradouro + ', ' + cep;
-            location_info.data = logradouro + ', ' + cep;
+              $scope.user_data = location_info;
 
-            $scope.location_info = location_info;
+              infowindow.setContent(location_info.full_address);
+              infowindow.open(map, marker);
 
-            console.log('$scope.location_info -> ', $scope.location_info);
+              console.log('$scope.user_data -> ', $scope.user_data);
+            } else {
+              alert('Sem resultados..');
+            }
           } else {
-            alert('Sem resultados..');
+            alert('Geocoder falhou por conta de: ' + status);
           }
-        } else {
-          alert('Geocoder falhou por conta de: ' + status);
-        }
-      });
+        });
 
-      // executa o mapa passando a função success quando o dom fizer o load
-      google.maps.event.addDomListener(window, 'load', success);
-    };
+        google.maps.event.addDomListener(window, 'load', success);
+      };
 
-    function error(error) {
-      console.warn(error);
-    };
+      function error(error) {
+        console.warn(error);
+      };
 
-    $scope.getUserLocation = function() {
       getLocation();
     };
+    // ====
+
+    // enviar pedido
+    $scope.user_data = {};
+
+    $scope.hungry = function() {
+      console.log($scope.user_data);
+    };
+    // ====
 
   }]);
